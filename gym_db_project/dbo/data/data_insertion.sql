060fcd27d4f62a1d09cb02340fd34086e5c7c0a1
linas-mackevicius-sql <linas.a.mackevicius@gmail.com>
2026-08-29 11:28:15 +0300

    Add .gitignore and post-deployment data script
    - Include data_insertion.sql for seeding initial records
    - Update .sqlproj to reference the new post-deployment script
    - Exclude bin/, obj/, and .vscode/ from version control

---
+++ gym_db_project/dbo/data/data_insertion.sql
@@ -0,0 +1,7 @@
+-- This file contains SQL statements that will be executed after the build script.
+
+-- test
+
+INSERT INTO [gym].[cities] ([title])
+VALUES ('Vilnius'), ('Kaunas'), ('Klaipėda');
+
